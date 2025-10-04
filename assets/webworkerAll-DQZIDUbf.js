d700f46ee347e9db570880476324c7513f0271b5
BNJMO <bnjmo.med@gmail.com>
2025-10-04 19:21:01 +0100

    change base to ./

---
+++ assets/webworkerAll-DQZIDUbf.js
@@ -0,0 +1,1 @@
+import"./init-CSTx56Eb.js";import"./index-DPvNLOBj.js";import"./colorToUniform-DmtBy-2V.js";import"https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";import"https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
